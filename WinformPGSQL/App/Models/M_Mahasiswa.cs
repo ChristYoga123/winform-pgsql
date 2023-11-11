04a4dccea970d991d54f693b657f4a7ce867a0d8
ChristYoga123 <christianuswibisono@gmail.com>
2023-11-11 23:08:43 +0700

    [FEAT] add models

---
+++ WinformPGSQL/App/Models/M_Mahasiswa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinformPGSQL.App.Models
+{
+    internal class M_Mahasiswa
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        public string nama { get; set; }
+        public string nim { get; set; }
+        [ForeignKey("M_Prodi")]
+        public int prodi_id { get; set; }
+    }
+}
